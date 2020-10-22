4deaec9ab7f71f2a0a90f4fdb242cd16f9eb764c
sebastiankluwa <sebastiankluwa@gmail.com>
2020-10-22 11:30:04 +0200

    Model, view, urls..

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from djang.core.urlresolvers import reverse
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -15,9 +16,23 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                                self.publish.strftime('%m'),
+                                self.publish.strftime('%d'),
+                                self.slug])
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
