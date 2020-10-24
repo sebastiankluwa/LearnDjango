c4f47cf4bea55482e0572414eb931178fb945eca
Sebastian <sebastiankluwa@gmail.com>
2020-10-24 13:14:19 +0200

    Comments

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    #pobranie posta na podstawie jego identyfikatora
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #weryfikacja pól formularza zakończyła się powodzeniem..
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = '{} ({}) zachęca do przeczytania "{}"'.format(cd['name'],
+            cd['email'], post.title)
+            message = 'Przeczytaj post "{}" na stronie {}\n\n Komentarz dodany przez {}: {}'.format(post.title,
+                                                                                                    post_url,
+                                                                                                    cd['name'],
+                                                                                                    cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -27,4 +55,21 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                                                                 publish__month=month,
                                                                 publish__day = day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    #Lista aktywnych komentarzy dla danego posta
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        #komentarz zostal opublikowany
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #utworzenie obiektu comment, bez zapisu do bazy
+            new_comment = comment_form.save(commit=False)
+            #przypisanie komentarza do postu
+            new_comment.post = post
+            #zapisanie komentarza do bazy
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 
+                                                                    'comment_form':comment_form})
